f57be55bc9592bbc4c912844e251bef294c30dfd
Risa Yamamoto <yamari2422@gmail.com>
2024-10-16 16:41:31 -0700

    Finished exercises

--- exercises/ex02.js
+++ exercises/ex02.js
@@ -3,16 +3,53 @@
 // Remove customers whose total cost is less than or equal to $50 from the queue.
 // Make sure to implement FIFO (First-In, First-Out)
 
-const Queue = require('../lib/Queue')
+const Queue = require("../lib/Queue");
 
 function groceryCheckout(queue) {
   // your code here
+  const temp = new Queue();
+  let totalNum;
+
+  while (!queue.isEmpty()) {
+    let filteredCustomer = queue.dequeue();
+    totalNum = filteredCustomer.cart.reduce((acc, cur) => {
+      return acc + cur.price;
+    }, 0);
+
+    if (totalNum > 50) {
+      temp.enqueue(filteredCustomer);
+    }
+  }
+
+  while (!temp.isEmpty()) {
+    queue.enqueue(temp.dequeue());
+  }
+
+  return queue;
 }
 
 const customers = new Queue();
-customers.push({ name: "Alice", cart: [{ item: "Milk", price: 10 }, { item: "Bread", price: 5 }] });
-customers.push({ name: "Bob", cart: [{ item: "Laptop", price: 500 }, { item: "Mouse", price: 20 }] });
-customers.push({ name: "Charlie", cart: [{ item: "Candy", price: 2 }, { item: "Juice", price: 3 }] });
+customers.enqueue({
+  name: "Alice",
+  cart: [
+    { item: "Milk", price: 10 },
+    { item: "Bread", price: 5 },
+  ],
+});
+customers.enqueue({
+  name: "Bob",
+  cart: [
+    { item: "Laptop", price: 500 },
+    { item: "Mouse", price: 20 },
+  ],
+});
+customers.enqueue({
+  name: "Charlie",
+  cart: [
+    { item: "Candy", price: 2 },
+    { item: "Juice", price: 3 },
+  ],
+});
 
 groceryCheckout(customers);
 console.log(customers.printQueue());
